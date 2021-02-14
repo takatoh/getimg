fb8ce6cec04439118368932f816eb2e1b48428a3
takatoh <takatoh.m@gmail.com>
2021-02-14 15:33:11 +0900

    getimg/getimg.py: main() method.

--- getimg/getimg.py
+++ getimg/getimg.py
@@ -80,80 +80,83 @@ def err_print(message):
     sys.stderr.write(message + "\n")
 
 
-parser = argparse.ArgumentParser(description="Download images from web page.")
-parser.add_argument('url', metavar='URL', action='store',
-                    help='specify URL')
-parser.add_argument('-v', '--version', action='version', version=script_version,
-                    help='show version and exit')
-group = parser.add_mutually_exclusive_group()
-group.add_argument('-e', '--embeded-image', dest='embeded', action='store_true',
-                   help='download embeded images(default)')
-group.add_argument('-l', '--linked-image', dest='linked', action='store_true',
-                   help='download linked images')
-parser.add_argument('-d', '--dir', dest='dir', metavar='DIR', action='store',
-                    help='download into DIR')
-parser.add_argument('-t', '--tags', dest='tags', metavar='TAGS', action='store', default='',
-                    help='put tags. use with -s option')
-parser.add_argument('-u', '--user-agent', dest='user_agent', metavar='AGENT', action='store',
-                    help='specify user-agent')
-parser.add_argument('-i', '--input-url', dest='input_url', metavar='URLLIST', action='store',
-                    help='input image url list from file')
-parser.add_argument('-s', '--sombrero', dest='sombrero', action='store_true',
-                    help='output yaml to post to Sombrero')
-parser.add_argument('-D', '--dump', dest='dump', action='store_true',
-                    help='dump image url instead download it')
-parser.add_argument('-H', '--html-dump', dest='html', action='store_true',
-                    help='dump html source')
-parser.add_argument('-n', '--no-download', dest='no_dl', action='store_true',
-                    help='not download images')
-args = parser.parse_args()
+def main():
+    parser = argparse.ArgumentParser(description="Download images from web page.")
+    parser.add_argument('url', metavar='URL', action='store',
+                        help='specify URL')
+    parser.add_argument('-v', '--version', action='version', version=script_version,
+                        help='show version and exit')
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('-e', '--embeded-image', dest='embeded', action='store_true',
+                       help='download embeded images(default)')
+    group.add_argument('-l', '--linked-image', dest='linked', action='store_true',
+                       help='download linked images')
+    parser.add_argument('-d', '--dir', dest='dir', metavar='DIR', action='store',
+                        help='download into DIR')
+    parser.add_argument('-t', '--tags', dest='tags', metavar='TAGS', action='store', default='',
+                        help='put tags. use with -s option')
+    parser.add_argument('-u', '--user-agent', dest='user_agent', metavar='AGENT', action='store',
+                        help='specify user-agent')
+    parser.add_argument('-i', '--input-url', dest='input_url', metavar='URLLIST', action='store',
+                        help='input image url list from file')
+    parser.add_argument('-s', '--sombrero', dest='sombrero', action='store_true',
+                        help='output yaml to post to Sombrero')
+    parser.add_argument('-D', '--dump', dest='dump', action='store_true',
+                        help='dump image url instead download it')
+    parser.add_argument('-H', '--html-dump', dest='html', action='store_true',
+                        help='dump html source')
+    parser.add_argument('-n', '--no-download', dest='no_dl', action='store_true',
+                        help='not download images')
+    args = parser.parse_args()
 
+    url = args.url
 
-url = args.url
+    if args.user_agent:
+        class Iopener(urllib.FancyURLopener):
+            version = args.user_agent
+        urllib._urlopener = Iopener()
 
-if args.user_agent:
-    class Iopener(urllib.FancyURLopener):
-        version = args.user_agent
-    urllib._urlopener = Iopener()
-
-if args.dir and not args.dump:
-    try:
-        os.makedirs(args.dir)
-    except OSError:
-        err_print("Directory exist: " + args.dir)
-        exit()
-err_print("Download images from " + url + "\n")
-if args.input_url:
-    log = []
-    f = open(args.input_url)
-    for image in f:
-        image = image.rstrip()
-        info = get_image(image)
-        log.append(info)
-        print(image)
-else:
-    try:
-        res = urlopen(url).read()
-    except IOError:
-        err_print("Error: failed to retrieve the page: " + url)
-        exit()
-    soup = BeautifulSoup(res, "lxml")
-    if args.html:
-        print(soup.prettify())
-        exit()
-    if args.linked:
-        log = get_linked_images(soup)
-    else:
-        log = get_embeded_images(soup)
-
-if not args.dump:
-    err_print("\n" + str(len(log)) + " images downloaded.")
-
-if args.sombrero:
-    yamlfile = 'images.yaml'
     if args.dir and not args.dump:
-         yamlfile = os.path.join(args.dir, yamlfile)
-    f = open(yamlfile, 'w')
-    f.write(yaml.dump(log))
-    err_print("\nOutput log to " + yamlfile + ".")
+        try:
+            os.makedirs(args.dir)
+        except OSError:
+            err_print("Directory exist: " + args.dir)
+            exit()
+    err_print("Download images from " + url + "\n")
+    if args.input_url:
+        log = []
+        f = open(args.input_url)
+        for image in f:
+            image = image.rstrip()
+            info = get_image(image)
+            log.append(info)
+            print(image)
+    else:
+        try:
+            res = urlopen(url).read()
+        except IOError:
+            err_print("Error: failed to retrieve the page: " + url)
+            exit()
+        soup = BeautifulSoup(res, "lxml")
+        if args.html:
+            print(soup.prettify())
+            exit()
+        if args.linked:
+            log = get_linked_images(soup)
+        else:
+            log = get_embeded_images(soup)
 
+    if not args.dump:
+        err_print("\n" + str(len(log)) + " images downloaded.")
+
+    if args.sombrero:
+        yamlfile = 'images.yaml'
+        if args.dir and not args.dump:
+             yamlfile = os.path.join(args.dir, yamlfile)
+        f = open(yamlfile, 'w')
+        f.write(yaml.dump(log))
+        err_print("\nOutput log to " + yamlfile + ".")
+
+
+
+main()
