6014a2dfc0c5c25162f9426ba826a1913b444b17
takatoh <takatoh.m@gmail.com>
2025-03-22 14:44:10 +0900

    getimg/getimg.py: Simplify codes to get images.

--- getimg/getimg.py
+++ getimg/getimg.py
@@ -177,15 +177,9 @@ def get_linked_images(soup, opts):
         if "href" not in a.attrs:
             continue
         image = a["href"]
-        if RE_IMAGE.match(image):
-            image = build_image_url(opts["url"], image)
-            print(image)
-            if not opts["isdump"]:
-                try:
-                    info = get_image(image, opts)
-                    images_list.append(info)
-                except IOError:
-                    err_print("Error: failed to retrieve the image.")
+        info = get_image(image, opts)
+        if info:
+            images_list.append(info)
     return images_list
 
 
@@ -193,28 +187,34 @@ def get_embeded_images(soup, opts):
     images_list = []
     for i in soup("img"):
         image = i["src"]
-        if RE_IMAGE.match(image):
-            image = build_image_url(opts["url"], image)
-            print(image)
-            if not opts["isdump"]:
-                try:
-                    info = get_image(image, opts)
-                    images_list.append(info)
-                except IOError:
-                    err_print("Error: failed to retrieve the image.")
+        info = get_image(image, opts)
+        if info:
+            images_list.append(info)
     return images_list
 
 
 def get_image(image, opts):
-    file = url_to_filename(image, opts["dir"])
-    if not (opts["isdump"] or opts["no_dl"]):
-        urlretrieve(image, file)
-    return {
-        "file": str(os.path.basename(file)),
-        "url": str(image),
-        "page_url": opts["url"],
-        "tags": opts["tags"],
-    }
+    if RE_IMAGE.match(image):
+        image = build_image_url(opts["url"], image)
+        print(image)
+        if not opts["isdump"]:
+            try:
+                file = url_to_filename(image, opts["dir"])
+                if not opts["no_dl"]:
+                    urlretrieve(image, file)
+                return {
+                    "file": str(os.path.basename(file)),
+                    "url": str(image),
+                    "page_url": opts["url"],
+                    "tags": opts["tags"],
+                }
+            except IOError:
+                err_print("Error: failed to retrieve the image.")
+                return None
+        else:
+            return None
+    else:
+        return None
 
 
 def url_to_filename(url, dir):
