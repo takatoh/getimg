6c02fa1416d98dceb422800ba60ee86448a67dad
takatoh <takatoh.m@gmail.com>
2025-08-12 05:53:43 +0900

    getimg/cli.py: Remove unnecessary imports.

--- getimg/cli.py
+++ getimg/cli.py
@@ -1,14 +1,10 @@
 import urllib
-from urllib.request import urlopen
-from bs4 import BeautifulSoup
 import yaml
 import os
 import argparse
 from . import (
     __version__,
     err_print,
-    get_linked_images,
-    get_embeded_images,
     get_image,
 )
 from . import pageparsers
